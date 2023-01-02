1d2a622694b833669ce1836d9f7bf703e3e9d76a
DhimanAirao <dhimanairao@gmail.com>
2023-01-02 15:12:27 +0530

    My Setup file Ubuntu

---
+++ my_setup_apt.sh
@@ -0,0 +1,7 @@
+sudo apt install micro
+sudo apt install snap
+sudo snap install gotop-cjbassi
+sduo snap install bottom
+sudo apt install guake
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
